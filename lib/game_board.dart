878aff09defa988d38f38880d0d9271b06c6f65f
woogi <dbwhddnr00@naver.com>
2023-10-02 23:25:18 +0900

    블럭 내려가기

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,5 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tetris/piece.dart';
 import 'package:tetris/pixel.dart';
+import 'package:tetris/values.dart';
+
+List<List<Tetromino?>> gameBoard =
+    List.generate(colLength, (index) => List.generate(rowLength, (j) => null));
 
 class GameBoard extends StatefulWidget {
   const GameBoard({Key? key}) : super(key: key);
@@ -9,8 +16,14 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  int rowLength = 10;
-  int colLength = 15;
+  Piece currentPiece = Piece(type: Tetromino.L);
+
+  @override
+  void initState() {
+    super.initState();
+
+    startGame();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,12 +31,69 @@ class _GameBoardState extends State<GameBoard> {
       backgroundColor: Colors.black,
       body: GridView.builder(
           itemCount: rowLength * colLength,
-          physics: NeverScrollableScrollPhysics(),
+          physics: const NeverScrollableScrollPhysics(),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: rowLength),
-          itemBuilder: (context, index) => Center(
-                child: Pixel(color: Colors.amberAccent),
-              )),
+          itemBuilder: (context, index) {
+            if (currentPiece.position.contains(index)) {
+              return Pixel(
+                color: Colors.yellow,
+                child: index.toString(),
+              );
+            } else {
+              return Pixel(
+                color: Colors.grey[900],
+                child: index.toString(),
+              );
+            }
+          }),
     );
+  }
+
+  void startGame() {
+    currentPiece.initializePiece();
+
+    Duration frameRate = const Duration(milliseconds: 800);
+    gameLoop(frameRate);
+  }
+
+  void gameLoop(Duration frameRate) {
+    Timer.periodic(frameRate, (timer) {
+      setState(() {
+        currentPiece.movePiece(Direction.down);
+      });
+    });
+  }
+
+  bool checkCollision(Direction direction) {
+    for (int i = 0; i < currentPiece.position.length; i++) {
+      int row = (currentPiece.position[i] / rowLength).floor();
+      int col = currentPiece.position[i] % rowLength;
+
+      if (direction == Direction.left) {
+        col -= 1;
+      } else if (direction == Direction.right) {
+        col += 1;
+      } else if (direction == Direction.down) {
+        row += 1;
+      }
+
+      if (row >= colLength || col < 0 || col >= rowLength) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void checkLanding() {
+    if (checkCollision(Direction.down)) {
+      for (int i = 0; i < currentPiece.position.length; i++) {
+        int row = (currentPiece.position[i] / rowLength).floor();
+        int col = currentPiece.position[i] % rowLength;
+        if (row >= 0 && col >= 0) {
+          gameBoard[row][col] = currentPiece.type;
+        }
+      }
+    }
   }
 }
--- lib/pixel.dart
+++ lib/pixel.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 class Pixel extends StatelessWidget {
   final Color? color;
-  const Pixel({super.key, required this.color});
+  var child;
+  Pixel({super.key, required this.color, required this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: color),
-      margin: EdgeInsets.all(1),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(4)),
+      margin: const EdgeInsets.all(1),
+      child: Center(
+        child: Text(
+          child,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
